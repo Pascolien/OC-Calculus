8b9009bb34e7b1861e5721bac86d40652e344017
Schmitt <clement.flance@gmail.com>
2017-09-12 23:07:15 +0200

    Add mathematicals function

--- OC-Calculus-WCFService/OperationsArithmetiques.svc.cs
+++ OC-Calculus-WCFService/OperationsArithmetiques.svc.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace OC_Calculus_WCFService
 {
@@ -12,49 +14,109 @@ namespace OC_Calculus_WCFService
     {
         public double Plus(double nb1, double nb2)
         {
-            //TODO
+            return nb1 + nb2;
         }
 
         public double Moins(double nb1, double nb2)
         {
-            //TODO
+            return nb1 - nb2;
         }
 
         public double Diviser(double nb1, double nb2)
         {
-            //TODO
+            return nb1 / nb2;
         }
 
         public double Multiplier(double nb1, double nb2)
         {
-            //TODO
+            return nb1 * nb2;
         }
 
         public bool EstMultipleDe(int nb1, int nb2)
         {
-            //TODO
+            return (nb1 % nb2) == 0; 
         }
 
         public int ChaineDAdditionsEntieres(string chaine)
         {
-            //TODO
+
+            int resultat = 0;
+            string[] nombres = chaine.Split('+');
+
+            if (nombres.Length > 0) // Traite le cas d'une chaîne avec des +
+            {
+                foreach (string nombre in nombres)
+                {
+                    resultat += Convert.ToInt32(nombre);
+                }
+            } else
+            {
+                if (chaine != "") // Traite le cas d'un seul nombre
+                {
+                    resultat = Convert.ToInt32(chaine);
+                }
+            }
+            
+            return resultat;
         }
 
         public double ChaineDAdditionsReelles(string chaine)
         {
-            //TODO
+            double resultat = 0;
+            //With Regex pattern = new Regex("[,]|[.]");
+
+            chaine = chaine.Replace(",", "."); 
+            string[] nombres = chaine.Split('+');
+            
+            if(nombres.Length > 0)
+            {
+                foreach (string nombre in nombres)
+                {
+                    resultat += Convert.ToDouble(nombre);
+                }
+            } else
+            {
+                if(chaine != "")
+                {
+                    resultat = Convert.ToDouble(chaine);
+                }
+            }
+
+            return resultat;
         }
         public int ChaineDAdditionsEtDeSoustractionsEntieres(string chaine)
         {
-            //TODO
+            chaine = chaine.Replace("-", "+-");
+            return ChaineDAdditionsEntieres(chaine); 
         }
         public double ChaineDAdditionsEtDeSoustractionsReelles(string chaine)
         {
-            //TODO
+            chaine = chaine.Replace("-", "+-");
+            return ChaineDAdditionsReelles(chaine);
         }
         public int ChaineDAdditionsEtSoustractionsEtMultiplicationsEntieres(string chaine)
         {
-            //TODO
+            int resultat = 0;
+            
+            string[] nombresAdditionner = chaine.Split('+');
+            string[] nombresMultiplier;
+
+            if (nombresAdditionner.Length > 0)
+            {
+                foreach (string strNombres in nombresAdditionner)
+                {
+                    int resultatMultiplication = 0;
+                    nombresMultiplier = strNombres.Split('*'); 
+                    
+                    foreach (string nombres in nombresMultiplier)
+                    {
+                        resultatMultiplication *= Convert.ToInt32(nombres);
+                    }
+
+                    resultat += resultatMultiplication;
+                }
+            }
+            return resultat;
         }
     }
 }
